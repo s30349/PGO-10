727604b16b560bde612f227207a7a8c9591d1d54
s30349 <162422022+s30349@users.noreply.github.com>
2024-06-04 16:30:19 +0200

    Add files via upload

---
+++ pgo10/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Manager manager=new Manager("Jan","Kowalski",1990,2010,"PJATK",2000,100);
+    System.out.println(manager);
+        }
+    }
